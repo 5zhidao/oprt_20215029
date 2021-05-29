10a631c6ab308d0498268aa44ddc78fa2d622da6
5zhidao <32709747+5zhidao@users.noreply.github.com>
2021-05-29 17:28:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,9 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
+git clone https://github.com/tty228/luci-app-serverchan package/serverchan
+git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+git clone https://github.com/kenzok8/small package/small
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
